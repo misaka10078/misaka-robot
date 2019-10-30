714061055a1ba1701dffcdec88defe502c1e95e1
王轶人 <403828602@qq.com>
2019-10-30 23:42:13 +0800

    update

--- Native.Csharp/App/Event/Event_GroupMessage.cs
+++ Native.Csharp/App/Event/Event_GroupMessage.cs
@@ -9,16 +9,16 @@ namespace Native.Csharp.App.Event
     {
         public void ReceiveGroupMessage(object sender, CqGroupMessageEventArgs e)
         {
+            DateTime dt = DateTime.Now;
+            DateTime dtfinal = new DateTime(2020, 6, 7, 0, 0, 0);
+            TimeSpan outdt = dtfinal - dt;
             // 将收到的消息发送给来源群, 营造复读机的效果
             if (e.Message == "高考倒计时")
-            {
-                DateTime dt = DateTime.Now;
-                DateTime dtfinal = new DateTime(2020, 6, 7, 0, 0, 0);
-                var outdt = dtfinal - dt;
+            {           
                 var outputmessage = "距离2020高考还有" + outdt.Days + "天" +
                 outdt.Hours + "小时" + outdt.Minutes + "分钟" + outdt.Seconds + "秒";
-                long QQID = 2366325788;
-                Common.CqApi.SendGroupMessage(e.FromGroup, Common.CqApi.CqCode_At(QQID, true) + Usual.languagemod1_start
+                //long QQID = 2366325788;
+                Common.CqApi.SendGroupMessage(e.FromGroup, Common.CqApi.CqCode_At(e.FromQQ, true) + Usual.languagemod1_start
                     + outputmessage+ Usual.languagemod1_over);
             }
         }
